dde119dc558042b0057a1ffe2b54325710c6143e
sgoogal <sangamesh.googal@lowes.com>
2020-08-24 23:35:10 +0530

    added further code for the user api

--- domain/users/user_dao.go
+++ domain/users/user_dao.go
@@ -2,32 +2,107 @@ package users
 
 import (
 	"fmt"
+	"github.com/sgoogal/bookstore_users-api/datasources/mysql/users_db"
 	"github.com/sgoogal/bookstore_users-api/utils/errors"
+	"github.com/sgoogal/bookstore_users-api/utils/mysql_utils"
 )
 
-var userDB = make(map[int64]*User)
+const (
+queryInsertUser = "INSERT INTO users(first_name, last_name, email, date_created, status, password) VALUES(?, ?, ?, ?, ?, ?);"
+queryGetUser = "SELECT id, first_name, last_name, email, date_created, status FROM users WHERE id=?;"
+queryUpdateUser = "UPDATE users SET first_name=?, last_name=?, email=? WHERE id=?;"
+queryDeleteUser = "DELETE FROM users WHERE id=?;"
+queryFindUserByStatus = "SELECT id, first_name, last_name, email, date_created, status FROM users WHERE status=?;"
+)
 
 func (user *User) Get() *errors.RestErr {
-	result := userDB[user.Id]
-	if result == nil {
-		return errors.NewNotFoundError(fmt.Sprintf("user %d not found", user.Id))
+	stmt, err := users_db.Client.Prepare(queryGetUser)
+	if err != nil {
+		return errors.NewInternalServerError(err.Error())
 	}
-	user.Id = result.Id
-	user.Email = result.Email
-	user.FirstName = result.FirstName
-	user.LastName = result.LastName
-	user.DateCreated = result.DateCreated
+    defer stmt.Close()
+
+	result := stmt.QueryRow(user.Id)
+	if getErr := result.Scan(&user.Id, &user.FirstName, &user.LastName, &user.Email, &user.DateCreated, &user.Status); getErr != nil {
+		return mysql_utils.ParseError(getErr)
+	}
 	return nil
 }
 
+
 func (user *User) Save() *errors.RestErr {
-	current := userDB[user.Id]
-	if current != nil {
-		if current.Email == user.Email {
-			return errors.NewBadRequestError(fmt.Sprintf("email %s already Registered", user.Email))
-		}
-		return errors.NewBadRequestError(fmt.Sprintf("user %d already exist", user.Id))
+	 stmt, err := users_db.Client.Prepare(queryInsertUser)
+	 if err != nil {
+	 	return errors.NewInternalServerError(err.Error())
 	}
-	userDB[user.Id] = user
+	defer stmt.Close()
+
+	 //user.DateCreated = date_utils.GetNowString()
+
+	insertResult, saveErr := stmt.Exec(user.FirstName, user.LastName, user.Email, user.DateCreated, user.Status, user.Password)
+	if saveErr != nil {
+		return mysql_utils.ParseError(saveErr)
+	}
+
+	userId, err := insertResult.LastInsertId()
+	if err != nil {
+		return mysql_utils.ParseError(err)
+	}
+	user.Id = userId
 	return nil
+}
+
+func (user *User) Update() *errors.RestErr {
+	stmt, err := users_db.Client.Prepare(queryUpdateUser)
+	if err != nil {
+		return errors.NewInternalServerError(err.Error())
+	}
+
+	_, err = stmt.Exec(user.FirstName, user.LastName, user.Email, user.Id)
+	if err != nil {
+		return mysql_utils.ParseError(err)
+	}
+	return nil
+}
+
+func (user *User) Delete() *errors.RestErr {
+	stmt, err := users_db.Client.Prepare(queryDeleteUser)
+	if err != nil {
+		return errors.NewInternalServerError(err.Error())
+	}
+	defer stmt.Close()
+
+	if _, err = stmt.Exec(user.Id); err != nil {
+		return mysql_utils.ParseError(err)
+	}
+	return nil
+}
+
+func (user *User) FindByStatus(status string) ([]User, *errors.RestErr) {
+	stmt, err := users_db.Client.Prepare(queryFindUserByStatus)
+	if err != nil {
+		return nil, errors.NewInternalServerError(err.Error())
+	}
+	defer stmt.Close()
+
+	rows, err := stmt.Query(status)
+	if err != nil {
+		return nil, errors.NewInternalServerError(err.Error())
+	}
+	defer rows.Close()
+
+	results := make([]User, 0)
+	for rows.Next() {
+		var user User
+		if err := rows.Scan(&user.Id, &user.FirstName, &user.LastName, &user.Email, &user.DateCreated, &user.Status); err != nil {
+			return nil, mysql_utils.ParseError(err)
+		}
+		results = append(results, user)
+	}
+
+	if len(results) == 0 {
+		return nil, errors.NewNotFoundError(fmt.Sprintf("no users found matching status %s", status))
+
+	}
+	return results, nil
 }
--- services/users_service.go
+++ services/users_service.go
@@ -2,13 +2,16 @@ package services
 
 import (
 	"github.com/sgoogal/bookstore_users-api/domain/users"
+	"github.com/sgoogal/bookstore_users-api/utils/date_utils"
 	"github.com/sgoogal/bookstore_users-api/utils/errors"
 )
 
 func CreateUser(user users.User) (*users.User, *errors.RestErr) {
-	if err := user.Validate(); err!=nil{
+	if err := user.Validate(); err != nil{
 		return nil, err
 	}
+	user.Status = users.StatusActive
+	user.DateCreated = date_utils.GetNowDBFormat()
 	if err := user.Save(); err != nil {
 		return nil, err
 	}
@@ -21,4 +24,43 @@ func GetUser(userId int64) (*users.User, *errors.RestErr) {
 		return nil, err
 	}
 	return result, nil
+}
+
+func UpdateUser(isPartial bool, user users.User) (*users.User, *errors.RestErr) {
+	current, err := GetUser(user.Id)
+	if err != nil {
+		return nil, err
+	}
+
+	if isPartial {
+		if user.FirstName != "" {
+			current.FirstName = user.FirstName
+		}
+		if user.LastName != "" {
+			current.LastName = user.LastName
+		}
+		if user.Email != "" {
+			current.Email = user.Email
+		}
+
+	} else {
+		current.FirstName = user.FirstName
+		current.LastName = user.LastName
+		current.Email = user.Email
+	}
+
+	if err := current.Update(); err != nil {
+		return nil, err
+	}
+	return current, nil
+}
+
+func DeleteUser(userId int64) *errors.RestErr {
+	user := &users.User{Id: userId}
+	return user.Delete()
+}
+
+func Search(status string) ([]users.User, *errors.RestErr) {
+	dao := &users.User{}
+	return dao.FindByStatus(status)
 }
